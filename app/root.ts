ced0b7a84fbdc85c8177900da2c3be7c840283a9
Hendrixer <willscottmoss@gmail.com>
2016-03-16 11:30:46 -0500

    chore

---
+++ app/root.ts
@@ -0,0 +1,11 @@
+interface Application {
+  name: string;
+}
+
+class App implements Application {
+  constructor(public name: string) {
+    alert(this.name);
+  }
+}
+
+var app = new App('hello');
